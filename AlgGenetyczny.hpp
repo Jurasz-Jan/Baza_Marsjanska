0cdf8b598308c6848be20460a9a1f86d15683e81
Jurasz-Jan <jan.jurasz@student.uj.edu.pl>
2023-05-26 21:51:29 +0200

    Create AlgGenetyczny.hpp

---
+++ AlgGenetyczny.hpp
@@ -0,0 +1,20 @@
+struct Rozwiazanie {
+    std::vector<int> genotyp;
+};
+
+// to szkic na razei
+
+std::vector<Rozwiazanie> selekcja(const std::vector<Rozwiazanie>& populacja, double beta);
+{
+std::vector<Rozwiazanie> nowaPopulacja;
+	
+	
+	
+	
+}
+
+
+void mutacja(std::vector<Rozwiazanie>& populacja, double prawdopodobienstwoMutacji) {
+   
+    
+}
