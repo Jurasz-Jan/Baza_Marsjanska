1abf3f005b8beab2ddeb3e0645cac00fec1eb86d
Jurasz-Jan <jan.jurasz@student.uj.edu.pl>
2023-05-27 18:10:35 +0200

    First Draft

--- AlgGenetyczny.hpp
+++ AlgGenetyczny.hpp
@@ -1,20 +1,26 @@
-struct Rozwiazanie {
-    std::vector<int> genotyp;
-};
-
-// to szkic na razei
-
-std::vector<Rozwiazanie> selekcja(const std::vector<Rozwiazanie>& populacja, double beta);
-{
-std::vector<Rozwiazanie> nowaPopulacja;
-	
-	
-	
-	
-}
 
 
 void mutacja(std::vector<Rozwiazanie>& populacja, double prawdopodobienstwoMutacji) {
    
     
+}
+
+// n- number of habitats, 
+// z-number of resources
+// a-inputted parameter
+//initial population size= anz
+
+
+double getRandomDouble(double min, double max) {
+    return min + static_cast<double>(rand()) / RAND_MAX * (max - min);
+}
+
+
+
+class BaseManager(int n, int z)    
+{
+	std::vector<Habitat> habitats;
+	//krzyzowanie 
+	
+	//selekcja
 }
--- Baza.cpp
+++ Baza.cpp
@@ -1,54 +1,14 @@
-#include <iostream>
-#include <vector>
+int n_of_habitats=0;
 
 
-struct Module {
-    bool isGeneralPurpose; // Czy moduł jest ogólnego przeznaczenia
-    
+
+
+struct ChannelConnection {
+    int fromHabitatId;
+    int toHabitatId;
+   
 };
 
-
-struct Task {
-    
-};
+# muszę jakoś rozmnażać moje osobniki. Nie chce narzucać niczego,ale potrzebuję, by każdy Node miał id
 
 
-void addResources(std::vector<Module>& system, int numResources) {
-  
-}
-
-// Funkcja do generowania początkowego pokolenia genotypów
-std::vector<std::vector<int>> generateInitialGenotypes(int numVertices, int numResources, int populationSize) {
-    std::vector<std::vector<int>> genotypes;
-    // Logika generowania początkowego pokolenia genotypów
-    return genotypes;
-}
-
-// Funkcja do krzyżowania genotypów
-std::vector<std::vector<int>> crossover(const std::vector<std::vector<int>>& genotypes, float crossoverRate) {
-    std::vector<std::vector<int>> offspring;
-    // Logika krzyżowania genotypów
-	
-	
-	// 
-	
-	
-	
-	
-    return offspring;
-}
-
-// Funkcja do mutacji genotypów
-void mutate(std::vector<std::vector<int>>& genotypes, float mutationRate) {
-    // Logika mutacji genotypów
-}
-
-// Funkcja do selekcji najlepszych genotypów
-std::vector<std::vector<int>> selection(const std::vector<std::vector<int>>& genotypes, int numSelections) {
-    std::vector<std::vector<int>> selectedGenotypes;
-    // Logika selekcji najlepszych genotypów
-    return selectedGenotypes;
-}
-
-// Funkcja mapująca genotypy na fenotypy i obliczająca koszt rozwiązania
-void mapGenotypesToFenotypes(const std::vector<std::vector<int>>& genotypes) {
